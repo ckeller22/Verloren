0a348640af5dc33310d2a405638384471a894580
ckeller22 <chriskeller222@gmail.com>
2020-10-14 10:23:08 -0700

    Inital commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Horizontal Movement")]
+    public float moveSpeed = 10f;
+    public Vector2 direction;
+
+    [Header("Components")]
+    public Rigidbody2D rigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(direction.x);
+    }
+
+    public void moveCharacter(float horizontal)
+    {
+        rigidBody2D.AddForce(Vector2.right * horizontal * moveSpeed);
+    }
+}
